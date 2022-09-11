14e8adba8b54fe54657c5c13fafbde7eaa49e9b8
zemscky <aleksei.zemscky@yandex.ru>
2022-09-11 16:02:38 +0300

    Задача 1

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int age = 17;
+        if (age >= 18) {
+            System.out.println("Поздравляем с совершенолетием");
+        }
+        if (age < 18) {
+            System.out.println("Возраст совершеннолетия ещё не наступил, и нужно немного подождать");
+        }
+    }
+}
