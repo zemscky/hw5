8567c1bf1f7316ecf57e83c305c6f6204b5502bc
zemscky <aleksei.zemscky@yandex.ru>
2022-09-11 16:40:15 +0300

    Задача 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+        // Задача 1
+        System.out.println("Задача 1");
         int age = 17;
         if (age >= 18) {
             System.out.println("Поздравляем с совершенолетием");
@@ -7,5 +9,21 @@ public class Main {
         if (age < 18) {
             System.out.println("Возраст совершеннолетия ещё не наступил, и нужно немного подождать");
         }
+        // Задача 2
+        System.out.println("Задача 2");
+        int age1 = 17;
+        if (age1 >=7)
+        if (age1 < 18) {
+            System.out.println("Ребенок ходит в школу");
+        }
+        if (age1 >= 18)
+        if (age1 < 24) {
+            System.out.println("Человек уже закончил школу и может отправляться в университет");
+        }
+        if (age1 >= 24) {
+            System.out.println("Человек окончил университет и ему пора искать первую работу");
+        }
+        // Задача 3
+        System.out.println("Задача 3");
     }
 }
